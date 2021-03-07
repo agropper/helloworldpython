cbf280e4a055b945b003f4b3794a7e4f9d1fb22f
ADRIAN GROPPER <agropper@healthurl.com>
2021-03-07 14:14:07 -0500

    add charset

--- server.py
+++ server.py
@@ -8,6 +8,7 @@ from http import HTTPStatus
 class Handler(http.server.SimpleHTTPRequestHandler):
     def do_GET(self):
         self.send_response(HTTPStatus.OK)
+        self.send_header(charset, "utf-8")
         self.end_headers()
         msg = 'Hello again! Do you want a Trustee? request for %s' % (self.path)
         self.wfile.write(msg.encode())
